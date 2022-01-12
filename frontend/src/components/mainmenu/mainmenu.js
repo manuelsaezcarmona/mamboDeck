54d131d837af469682dc222335f4b62b3e861ae9
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-01-12 13:51:56 +0100

    Added general menu container with styles

--- frontend/src/components/mainmenu/mainmenu.js
+++ frontend/src/components/mainmenu/mainmenu.js
@@ -6,18 +6,12 @@ export function MainMenu() {
     <nav className="mainmenu-nav">
       <ul className="mainmenu-nav__list">
         <li className="mainmenu-nav__item">
-          <Link className="mainmenu-nav__link" to="/">
-            <span className="mainmanu-nav__span mainmanu-nav__span--before">Mambo</span>
-            <span className="mainmanu-nav__span mainmanu-nav__span--after">Deck</span>
-          </Link>
-        </li>
-        <li>
           <Link className="mainmenu-nav__link" to="/register">Registro</Link>
         </li>
-        <li>
+        <li className="mainmenu-nav__item">
           <Link className="mainmenu-nav__link" to="/login">Login</Link>
         </li>
-        <li>
+        <li className="mainmenu-nav__item">
           <Link className="mainmenu-nav__link" to="/user">Tu cuenta</Link>
         </li>
       </ul>
